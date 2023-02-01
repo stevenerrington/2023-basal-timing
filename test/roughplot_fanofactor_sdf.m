778d03253ad91439537d26a047a6be3892fc9c36
Steven Errington <steven.errington@outlook.com>
2023-02-01 15:56:18 -0600

    Updated fig check
    Updated figure check - SDF and fano factor x time, with relevant events marked. Seems functional

--- test/roughplot_fanofactor_sdf.m
+++ test/roughplot_fanofactor_sdf.m
@@ -7,7 +7,9 @@ for neuron_i = 1:size(bf_data,1)
     plot(-5000:5000,nanmean(bf_data.sdf{neuron_i}(bf_data.trials{neuron_i}.prob50,:)))
     plot(-5000:5000,nanmean(bf_data.sdf{neuron_i}(bf_data.trials{neuron_i}.prob75,:)))
     plot(-5000:5000,nanmean(bf_data.sdf{neuron_i}(bf_data.trials{neuron_i}.prob100,:)))
-    xlim([-500 3000]); vline(0, 'k'); vline(1000,'k'); vline(1000+1500,'k')
+    xlim([-1500 3000]); vline(-1000,'k'); vline(0, 'k'); 
+    
+    if strcmp(bf_datasheet.site{neuron_i},'wustl'); vline(2500,'k'); else; vline(1500,'k'); end
     
     subplot(2,1,2); hold on
     plot(bf_data.fano(neuron_i).time,bf_data.fano(neuron_i).FanoSave100)
@@ -15,7 +17,8 @@ for neuron_i = 1:size(bf_data,1)
     plot(bf_data.fano(neuron_i).time,bf_data.fano(neuron_i).FanoSave50)
     plot(bf_data.fano(neuron_i).time,bf_data.fano(neuron_i).FanoSave25)
     plot(bf_data.fano(neuron_i).time,bf_data.fano(neuron_i).FanoSave0)
-    xlim([-500 3000]); ylim([0 4]); hline(1,'k'), vline(-1000, 'k'); vline(0,'k'); vline(1000+1500,'k')
+    xlim([-1500 3000]); ylim([0 4]); hline(1,'k'), vline(-1000, 'k'); vline(0,'k');
+    if strcmp(bf_datasheet.site{neuron_i},'wustl'); vline(2500,'k'); else; vline(1500,'k'); end
     
 end
 
