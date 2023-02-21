283f48b979da7a93c8165fbecd3d9bce3668dceb
Steven Errington <steven.errington@outlook.com>
2023-02-20 20:37:46 -0600

    Added gray array devs

--- basalforebrain_primary.m
+++ basalforebrain_primary.m
@@ -16,8 +16,15 @@ pavlovian_bf_master % Pavlovian task data
 timing_bf_master    % Timing task data
 trace_bf_master     % Trace task data
 
+%%%% < eye-tracking: plot eye position x time, and sdf
+
+
 % Striatum: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 pavlovian_striatum_master
+
+
+% Brainwide: >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+get_gray_datamap
 
 %% Figures: 
 figure_primary
