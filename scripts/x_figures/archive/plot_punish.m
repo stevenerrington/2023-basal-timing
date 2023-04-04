bd9e3131eba8ff1af508c8bf91f6dfd5f2c74da1
Steven Errington <steven.errington@outlook.com>
2023-04-04 12:00:14 -0500

    Update fig 1 ideas

---
+++ scripts/x_figures/archive/plot_punish.m
@@ -0,0 +1,125 @@
+%% Punish
+% Initialize plot data structures
+plot_sdf_data = [];
+plot_spk_data = [];
+plot_fano_data = []; plot_fano_label = [];
+plot_label = [];
+plot_time = [-5000:5000];
+time_zero = abs(plot_time(1));
+
+plot_trial_types = trialtype_plot.punish;
+
+% Raster & SDF restructuring
+for trial_type_i = 1:length(plot_trial_types)
+    trial_type_label = plot_trial_types{trial_type_i};
+    trials_in = []; trials_in = data_in.trials{example_neuron_i}.(trial_type_label);
+    n_trls = size(trials_in,2);
+    
+    plot_sdf_data = [plot_sdf_data ; num2cell(data_in.sdf{example_neuron_i}(trials_in,:),2)];
+    plot_label = [plot_label; repmat({[int2str(trial_type_i) '_' (trial_type_label)]},n_trls,1)];
+    
+    spkTimes = {};
+    for trl_i = 1:n_trls
+        spkTimes{trl_i,1} = find(data_in.rasters{example_neuron_i}(trials_in(trl_i),:) == 1) - time_zero;
+    end
+    
+    plot_spk_data = [plot_spk_data; spkTimes];
+    
+    
+    plot_fano_data = [plot_fano_data; {data_in.fano(example_neuron_i).raw.(trial_type_label)}];
+    plot_fano_label = [plot_fano_label; {[int2str(trial_type_i) '_' (trial_type_label)]}];
+end
+
+% Example neuron %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% CS Onset ----------------------------------------------------------------
+% Raster plot
+figure_plot(1,1)=gramm('x',plot_spk_data,'color',plot_label);
+figure_plot(1,1).geom_raster();
+figure_plot(1,1).axe_property('XLim',xlim_input_CS,'XTick',[],'YTick',[],'XColor',[1 1 1],'YColor',[1 1 1]);
+figure_plot(1,1).set_names('x','','y','');
+figure_plot(1,1).set_color_options('map',color_scheme_punish);
+figure_plot(1,1).no_legend;
+
+% Spike density function
+figure_plot(2,1)=gramm('x',plot_time,'y',plot_sdf_data,'color',plot_label);
+figure_plot(2,1).stat_summary();
+figure_plot(2,1).axe_property('XLim',xlim_input_CS,'YLim',ylim_input,'XTick',[],'XColor',[1 1 1]);
+figure_plot(2,1).set_names('x','','y','');
+figure_plot(2,1).set_color_options('map',color_scheme_punish);
+figure_plot(2,1).no_legend;
+
+% Fano factor
+figure_plot(3,1)=gramm('x',data_in.fano(example_neuron_i).time,'y',plot_fano_data,'color',plot_fano_label);
+figure_plot(3,1).geom_line();
+figure_plot(3,1).axe_property('XLim',xlim_input_CS,'YLim',[0 2],'XTick',[],'XColor',[1 1 1]);
+figure_plot(3,1).set_names('x','','y','');
+figure_plot(3,1).set_color_options('map',color_scheme_punish);
+figure_plot(3,1).geom_hline('yintercept',1,'style','k--');
+figure_plot(3,1).no_legend;
+
+% Population -----------------------------------------------------
+% Initialize arrays
+plot_sdf_data_pop = []; plot_fano_data_pop = []; plot_fano_label_pop = [];
+plot_category_label_pop = []; plot_label_pop = [];
+
+% Define parameters
+plot_time = [-5000:5000]; time_zero = abs(plot_time(1));
+norm_window = 5000+[-1000:3000];
+
+% For each neuron
+for neuron_i = 1:size(data_in,1)
+    
+    baseline_trials = [];
+    for trial_type_i = 1:length(plot_trial_types)
+        trial_type_label = plot_trial_types{trial_type_i};        
+        baseline_trials = [baseline_trials,data_in.trials{neuron_i}.(trial_type_label)];
+    end
+    
+    % Get the mean and std to normalize
+    bl_fr_mean = nanmean(nanmean(data_in.sdf{neuron_i}(baseline_trials,norm_window)));
+    bl_fr_std = nanstd(nanmean(data_in.sdf{neuron_i}(baseline_trials,norm_window)));
+    
+    % For each trial type
+    for trial_type_i = 1:length(plot_trial_types)
+        trial_type_label = plot_trial_types{trial_type_i};
+        trials_in = []; trials_in = data_in.trials{neuron_i}.(trial_type_label);
+        
+        n_trls = size(trials_in,2);
+        
+        % Get the normalized SDF
+        sdf_x = []; sdf_x = (nanmean(data_in.sdf{neuron_i}(trials_in,:))-bl_fr_mean)./bl_fr_std;
+        fano_x = [];
+        
+        
+        fano_x = data_in.fano(neuron_i).raw.(trial_type_label);
+        sdf_out = []; sdf_out = sdf_x;
+
+        plot_sdf_data_pop = [plot_sdf_data_pop ; num2cell(sdf_out,2)];
+        plot_label_pop = [plot_label_pop; {[int2str(trial_type_i) '_' (trial_type_label)]}];
+        
+        plot_fano_data_pop = [plot_fano_data_pop; {fano_x}];
+        plot_fano_label_pop = [plot_fano_label_pop; {[int2str(trial_type_i) '_' (trial_type_label)]}];
+        
+        plot_category_label_pop = [plot_category_label_pop; {bf_datasheet_CS.cluster_label{neuron_i}}];
+        
+    end
+end
+
+% Population neuron %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% CS Onset ----------------------------------------------------------------
+% Spike density function
+figure_plot(4,1)=gramm('x',plot_time,'y',plot_sdf_data_pop,'color',plot_label_pop);
+figure_plot(4,1).stat_summary();
+figure_plot(4,1).axe_property('XLim',xlim_input_CS,'YLim',[-3 5],'XTick',[],'XColor',[1 1 1]);
+figure_plot(4,1).set_names('x','','y','');
+figure_plot(4,1).set_color_options('map',color_scheme_punish);
+figure_plot(4,1).no_legend;
+
+% Fano factor
+figure_plot(5,1)=gramm('x',data_in.fano(example_neuron_i).time,'y',plot_fano_data_pop,'color',plot_fano_label_pop);
+figure_plot(5,1).stat_summary();
+figure_plot(5,1).axe_property('XLim',xlim_input_CS,'YLim',[0 2]);
+figure_plot(5,1).set_names('x','Time from CS Onset (ms)','y','');
+figure_plot(5,1).set_color_options('map',color_scheme_punish);
+figure_plot(5,1).geom_hline('yintercept',1,'style','k--');
+figure_plot(5,1).no_legend;
--- test/mmff_test.m
+++ test/mmff_test.m
@@ -19,34 +19,138 @@ load striatum_datasheet_CS
 %       data and 800 ms of post-target data. Your 'spikes' matrix would then be 100 by 1000
 %       (with one of these for each element of 'data').
 
-clear ramping_neurons_list
-ramping_neurons_list = find(bf_datasheet_CS.cluster_id == 1);
-
-clear BFdata_test BGdata_test
-for neuron_i = 1:length(ramping_neurons_list)
-    
-    BFdata_test(neuron_i).spikes = logical(bf_data_CS.rasters{ramping_neurons_list(neuron_i)}...
-        (bf_data_CS.trials{ramping_neurons_list(neuron_i)}.uncertain,:));
-end
-
-for neuron_i = 1:size(striatum_datasheet_CS,1)
-    BGdata_test(neuron_i).spikes = logical(striatum_data_CS.rasters{neuron_i}...
-        (striatum_data_CS.trials{neuron_i}.uncertain,:));
-end
+clear ramping_neurons_list phasic_neurons_list
+ramping_neurons_list = find(bf_datasheet_CS.cluster_id == 2);
+phasic_neurons_list = find(bf_datasheet_CS.cluster_id == 1);
 
 
-% target onset is at 5000 ms
-clear figureParams fanoParams
+% Fano parameters -----------------
+times = 4500:50:7500; % from 800 ms before CS onset until 500 ms after reward.
 
-times = 4800:25:8000; % from 800 ms before CS onset until 500 ms after reward.
+fanoParams = [];
 fanoParams.alignTime = 5000; % this time will become zero time
 fanoParams.boxWidth = 25; % 50 ms sliding window.
-fanoParams.binSpacing = 0.25; % 50 ms sliding window.
+fanoParams.binSpacing = 0.5; % 50 ms sliding window.
+
 figureParams.plotRawF = 1;
 
-Result_BF = VarVsMean(BFdata_test, times, fanoParams);
-plotFano(Result_BF,figureParams);
+clear mmff_bf
+for neuron_i = 1:length(ramping_neurons_list)
+    if strcmp(bf_datasheet_CS.site{ramping_neurons_list(neuron_i)},'wustl')
+        timecut = [1:5750,6250:10001];
+        zeropad = zeros(length(bf_data_CS.trials{ramping_neurons_list(neuron_i)}.uncertain),10001-length(timecut));
+        
+        mmff_bf.data.ramping(neuron_i).spikes = [logical(bf_data_CS.rasters{ramping_neurons_list(neuron_i)}...
+        (bf_data_CS.trials{ramping_neurons_list(neuron_i)}.uncertain,timecut)),zeropad];
+    
+    else
+        mmff_bf.data.ramping(neuron_i).spikes = logical(bf_data_CS.rasters{ramping_neurons_list(neuron_i)}...
+        (bf_data_CS.trials{ramping_neurons_list(neuron_i)}.uncertain,:));
+    end
+    
 
-% 21% of datapoints survived matching (~75% in example)
+end
+
+for neuron_i = 1:length(phasic_neurons_list)
+    mmff_bf.data.phasic(neuron_i).spikes = logical(bf_data_CS.rasters{phasic_neurons_list(neuron_i)}...
+        (bf_data_CS.trials{phasic_neurons_list(neuron_i)}.uncertain,:));
+end
 
 
+
+mmff_bf.fano.ramping = VarVsMean(mmff_bf.data.ramping, times, fanoParams);
+mmff_bf.fano.phasic = VarVsMean(mmff_bf.data.phasic, times, fanoParams);
+
+%% 
+figure('Renderer', 'painters', 'Position', [100 100 800 600]);
+subplot(2,2,1); hold on
+plot(mmff_bf.fano.ramping.times',mmff_bf.fano.ramping.meanRateAll','color',[0.5 0.5 0.5],'LineWidth',2);
+plot(mmff_bf.fano.ramping.times',mmff_bf.fano.ramping.meanRateSelect','k','LineWidth',1);
+xlim([-500 2000]); vline([0 1500],'k-')
+
+title('Ramping')
+
+subplot(2,2,2); hold on
+plot(mmff_bf.fano.phasic.times',mmff_bf.fano.phasic.meanRateAll','color',[0.5 0.5 0.5],'LineWidth',2);
+plot(mmff_bf.fano.phasic.times',mmff_bf.fano.phasic.meanRateSelect','k','LineWidth',1);
+xlim([-500 2000]); vline([0 1500],'k-')
+
+title('Phasic')
+
+subplot(2,2,3); hold on
+
+plot_ci(mmff_bf.fano.ramping.times,...
+    [mmff_bf.fano.ramping.FanoFactor';... % mean
+    mmff_bf.fano.ramping.Fano_95CIs(:,1)';...
+    mmff_bf.fano.ramping.Fano_95CIs(:,2)'],...
+    'MainLineColor','r','PatchColor', 'r', 'PatchAlpha', 0.2,'LineColor','k');
+xlim([-500 2000]); ylim([0 3]); vline([0 1500],'k-')
+
+plot_ci(mmff_bf.fano.ramping.times,...
+    [mmff_bf.fano.ramping.FanoFactorAll';... % mean
+    mmff_bf.fano.ramping.FanoAll_95CIs(:,1)';...
+    mmff_bf.fano.ramping.FanoAll_95CIs(:,2)'],...
+    'MainLineColor',[0.5 0.5 0.5],'PatchColor',[0.5 0.5 0.5], 'PatchAlpha', 0.1,'LineColor','k');
+xlim([-500 2000]); ylim([0 3]); vline([0 1500],'k-')
+
+subplot(2,2,4); hold on
+
+plot_ci(mmff_bf.fano.phasic.times,...
+    [mmff_bf.fano.phasic.FanoFactor';... % mean
+    mmff_bf.fano.phasic.Fano_95CIs(:,1)';...
+    mmff_bf.fano.phasic.Fano_95CIs(:,2)'],...
+    'MainLineColor','b','PatchColor', 'b', 'PatchAlpha', 0.2,'LineColor','k');
+xlim([-500 2000]); ylim([0 3]); vline([0 1500],'k-')
+
+plot_ci(mmff_bf.fano.phasic.times,...
+    [mmff_bf.fano.phasic.FanoFactorAll';... % mean
+    mmff_bf.fano.phasic.FanoAll_95CIs(:,1)';...
+    mmff_bf.fano.phasic.FanoAll_95CIs(:,2)'],...
+    'MainLineColor',[0.5 0.5 0.5],'PatchColor',[0.5 0.5 0.5], 'PatchAlpha', 0.1,'LineColor','k');
+xlim([-500 2000]); ylim([0 3]); vline([0 1500],'k-')
+
+%%
+
+
+
+%%
+% n_neuron_bootstrap_sample = 25;
+% 
+% % Bootstrapped Fano Extraction ----------------
+% for bootstrap_i = 1:50
+%     bootstrap_i
+%     bootstrap_neuron_list = [];
+%     bootstrap_neuron_list = randsample(ramping_neurons_list,35);
+%     
+%     clear mmff_bf.ramping
+%     
+%     % Extract raster for bootstrapped neurons
+%     for neuron_i = 1:length(bootstrap_neuron_list)
+%         mmff_bf.ramping(neuron_i).spikes = logical(bf_data_CS.rasters{bootstrap_neuron_list(neuron_i)}...
+%             (bf_data_CS.trials{bootstrap_neuron_list(neuron_i)}.uncertain,:));
+%     end
+%     
+%     clear Result_BF
+%     Result_BF = VarVsMean(mmff_bf.ramping, times, fanoParams);
+%     
+%     fano_test(bootstrap_i,:) = Result_BF.FanoFactor';
+%     fano_time_test(bootstrap_i,:) = Result_BF.times';
+%     
+% 
+% end
+% 
+% fano_test;
+% 
+% % target onset is at 5000 ms
+% clear figureParams fanoParams
+% 
+% 
+% 
+% 
+% 
+% 
+% % 21% of datapoints survived matching (~75% in example)
+% 
+% 
+% 
+% 
