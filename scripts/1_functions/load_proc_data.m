562993bbbd611567ad71b8858836fc55895b6e49
Steven Errington <steven.errington@outlook.com>
2023-06-19 19:20:47 -0500

    Updated data mapping for mac use

--- scripts/1_functions/load_proc_data.m
+++ scripts/1_functions/load_proc_data.m
@@ -1,19 +1,30 @@
 %% Load processed data
-clc
-fprintf('Loading pre-extracted data \n')
-% Extracted beh & neurophys
-load(fullfile(dirs.root,'data','large','bf_data_CS.mat')) %bf_data_CS_spkRemoved
-load(fullfile(dirs.root,'data','large','bf_data_CS2.mat'))
-load(fullfile(dirs.root,'data','large','bf_data_timingTask.mat'))
-load(fullfile(dirs.root,'data','large','bf_data_traceExp.mat'))
-load(fullfile(dirs.root,'data','large','bf_data_punish.mat'))
-load(fullfile(dirs.root,'data','large','striatum_data_CS.mat'))
-load(fullfile(dirs.root,'data','large','striatum_data_traceExp.mat'))
-% Extracted datasheets
-load(fullfile(dirs.root,'data','large','bf_datasheet_CS.mat'))
-load(fullfile(dirs.root,'data','large','bf_datasheet_CS2.mat'))
-load(fullfile(dirs.root,'data','large','bf_datasheet_timingExp.mat'))
-load(fullfile(dirs.root,'data','large','bf_datasheet_traceExp.mat'))
-load(fullfile(dirs.root,'data','large','bf_datasheet_punish.mat'))
-load(fullfile(dirs.root,'data','large','striatum_datasheet_CS.mat'))
-load(fullfile(dirs.root,'data','large','striatum_datasheet_traceExp.mat'))
+clc; fprintf('Loading pre-extracted data \n')
+
+if ispc()
+    data_dir = fullfile(dirs.root,'data','large');
+end
+if ismac()
+    data_dir = '/Users/stevenerrington/Box Sync/Research/2023-basal-timing/data';
+end
+
+%% Load data:
+% Neurophysiology files
+load(fullfile(data_dir,'bf_data_CS.mat')) %bf_data_CS_spkRemoved
+load(fullfile(data_dir,'bf_data_CS2.mat'))
+load(fullfile(data_dir,'bf_data_timingTask.mat'))
+load(fullfile(data_dir,'bf_data_traceExp.mat'))
+load(fullfile(data_dir,'bf_data_punish.mat'))
+load(fullfile(data_dir,'striatum_data_CS.mat'))
+load(fullfile(data_dir,'striatum_data_traceExp.mat'))
+
+% Datasheet files
+load(fullfile(data_dir,'bf_datasheet_CS.mat'))
+load(fullfile(data_dir,'bf_datasheet_CS2.mat'))
+load(fullfile(data_dir,'bf_datasheet_timingExp.mat'))
+load(fullfile(data_dir,'bf_datasheet_traceExp.mat'))
+load(fullfile(data_dir,'bf_datasheet_punish.mat'))
+load(fullfile(data_dir,'striatum_datasheet_CS.mat'))
+load(fullfile(data_dir,'striatum_datasheet_traceExp.mat'))
+
+clear data_dir
